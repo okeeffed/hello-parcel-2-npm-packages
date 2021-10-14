a054945c3dd430df233ecabf109bc051f8c5a209
Dennis O'Keeffe <hello@dennisokeeffe.com>
2021-10-15 08:34:10 +1100

    feat: complete basics for Parcel 2 demo package

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hello Parcel 2 NPM Packages
+
+This repo is a placeholder for a blog post example of using [Parcel 2](https://parceljs.org/getting-started/library/) for creating an NPM package library.
+
+The blog post can be found [here](https://blog.dennisokeeffe.com/).
---
+++ src/math.ts
@@ -0,0 +1,35 @@
+/**
+ * Adds two numbers together.
+ *
+ * @param x First argument.
+ * @param y Second argument.
+ * @returns Result of addition.
+ */
+export const add = (x: number, y: number): number => x + y;
+
+/**
+ * Subtract the second argument from the first.
+ *
+ * @param x First argument.
+ * @param y Second argument.
+ * @returns Result of subtraction.
+ */
+export const subtract = (x: number, y: number): number => x - y;
+
+/**
+ * Multiplies two numbers together.
+ *
+ * @param x First argument.
+ * @param y Second argument.
+ * @returns Result of multiplication.
+ */
+export const multiply = (x: number, y: number): number => x * y;
+
+/**
+ * Divides the first argument by the second.
+ *
+ * @param x First argument.
+ * @param y Second argument.
+ * @returns Result of division.
+ */
+export const divide = (x: number, y: number): number => x / y;
